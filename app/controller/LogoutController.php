762139fa55b7fe55a647dc3317a76cac38bd224a
Erick Ferreira <erick.xferreira@gmail.com>
2021-04-09 16:35:10 -0300

    chore: logout status

--- app/controller/LogoutController.php
+++ app/controller/LogoutController.php
@@ -2,12 +2,44 @@
 
 namespace app\controller;
 
+use app\model\LogoutModel;
+use app\helpers\Transaction;
+use app\lib\LoggerHTML;
+use Exception;
+
 class LogoutController
 {
     public function __construct()
     {
         session_start();
+    
+        $id = $_SESSION['user-id'];
+        $this->updateStatus($id, 'false');
+
         unset($_SESSION['user-id']);
         header('location: /login');
+    }
+
+    private function updateStatus($id, $status)
+    {
+        try {
+            Transaction::open('db');
+            Transaction::setLogger( new LoggerHTML('log.html') );
+
+            $model = new LogoutModel();
+
+            $result = $model->update('users', 'status', 'id', $id, $status);
+
+            Transaction::close();
+
+            return $result;
+
+        } catch( Exception $e ) {
+
+            Transaction::log($e->getMessage());
+            Transaction::rollback();
+
+            return false;
+        }
     }
 }
---
+++ app/model/LogoutModel.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace app\model;
+
+use app\traits\ModelTrait;
+
+class LogoutModel
+{
+    use ModelTrait;
+}
